01ad77755ff37143b1018cf1a474c260690986d9
Tim Davis <timdavish@gmail.com>
2023-12-05 18:05:27 -0800

    feat: add mui theme and provider

---
+++ Frontend/src/theme/palette.ts
@@ -0,0 +1,10 @@
+import type {ThemeOptions} from '@mui/material'
+
+export const palette: ThemeOptions['palette'] = {
+  // primary: {
+  // main: '#ffffff',
+  // },
+  // secondary: {
+  // main: '#ffffff',
+  // },
+}
