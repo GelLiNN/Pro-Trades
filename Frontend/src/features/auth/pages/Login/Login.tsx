886e61cc3d3035dcab45b4ca06983c5a1a2cf486
Tim Davis <timdavish@gmail.com>
2023-12-10 16:19:04 -0800

    feat: add react-hook-form (#32)

--- Frontend/src/features/auth/pages/Login/Login.tsx
+++ Frontend/src/features/auth/pages/Login/Login.tsx
@@ -1,100 +1,104 @@
-import {useCallback, useState} from 'react'
+import {useCallback} from 'react'
 import {useNavigate} from 'react-router-dom'
+import * as zod from 'zod'
 import {useLoginMutation} from '@/features/auth/api'
 import {setCredentials} from '@/features/auth/state'
 import {addNotification} from '@/features/notifications/state'
 import {useDispatch} from '@/store'
 
-import {Box, Button, Grid, Link, TextField} from '@mui/material'
+import {Button, Grid, Link, TextField} from '@mui/material'
+import {Form} from '@/components/Form'
 import {AuthLayout} from '@/features/auth/components/AuthLayout'
 
-import type {ChangeEvent, FormEvent} from 'react'
 import type {LoginRequest} from '@/features/auth/api'
+
+interface LoginData extends LoginRequest {}
+
+const loginSchema = zod.object({
+  email: zod.string().min(1, 'Email is required'),
+  password: zod.string().min(1, 'Password is required'),
+})
 
 export const Login = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const [formState, setFormState] = useState<LoginRequest>({
-    email: '',
-    password: '',
-  })
-
   const [login, {isLoading}] = useLoginMutation()
 
-  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setFormState(formState => ({
-      ...formState,
-      [event.target.name]: event.target.value,
-    }))
-  }, [])
-
   const handleSubmit = useCallback(
-    async (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
-
+    async (loginData: LoginData) => {
       try {
-        const loginResponse = await login(formState).unwrap()
+        const loginResponse = await login(loginData).unwrap()
         dispatch(setCredentials(loginResponse))
+        dispatch(
+          addNotification({
+            message: 'Log in successful',
+            severity: 'success',
+          })
+        )
         navigate('/')
       } catch (error) {
         dispatch(
           addNotification({
-            message: 'Failed to login',
+            message: 'Failed to log in',
             severity: 'error',
           })
         )
       }
     },
-    [dispatch, formState, login, navigate]
+    [dispatch, login, navigate]
   )
 
   return (
     <AuthLayout title='Log In'>
-      <Box component='form' noValidate onSubmit={handleSubmit}>
-        <TextField
-          autoComplete='email'
-          autoFocus
-          fullWidth
-          id='email'
-          label='Email Address'
-          margin='normal'
-          name='email'
-          onChange={handleChange}
-          required
-          type='text'
-        />
+      <Form<LoginData, typeof loginSchema> onSubmit={handleSubmit} schema={loginSchema}>
+        {({formState, register}) => (
+          <>
+            <TextField
+              autoComplete='email'
+              autoFocus
+              error={!!formState.errors.email}
+              fullWidth
+              helperText={formState.errors.email?.message}
+              label='Email'
+              margin='normal'
+              required
+              type='text'
+              {...register('email')}
+            />
 
-        <TextField
-          autoComplete='current-password'
-          fullWidth
-          id='password'
-          label='Password'
-          margin='normal'
-          name='password'
-          onChange={handleChange}
-          required
-          type='password'
-        />
+            <TextField
+              autoComplete='current-password'
+              error={!!formState.errors.password}
+              fullWidth
+              helperText={formState.errors.password?.message}
+              label='Password'
+              margin='normal'
+              required
+              type='password'
+              {...register('password')}
+            />
 
-        <Button disabled={isLoading} fullWidth sx={{my: 2}} type='submit' variant='contained'>
-          Log In
-        </Button>
+            <Button disabled={isLoading} fullWidth sx={{my: 2}} type='submit' variant='contained'>
+              Log In
+            </Button>
 
-        <Grid container>
-          <Grid item xs>
-            <Link href='/auth/recover-password' variant='body2'>
-              Forgot your password?
-            </Link>
-          </Grid>
+            <Grid container>
+              <Grid item xs>
+                <Link href='/auth/recover-password' variant='body2'>
+                  Forgot your password?
+                </Link>
+              </Grid>
 
-          <Grid item>
-            <Link href='/auth/register' variant='body2'>
-              {"Don't have an account?"}
-            </Link>
-          </Grid>
-        </Grid>
-      </Box>
+              <Grid item>
+                <Link href='/auth/register' variant='body2'>
+                  {"Don't have an account?"}
+                </Link>
+              </Grid>
+            </Grid>
+          </>
+        )}
+      </Form>
     </AuthLayout>
   )
 }
