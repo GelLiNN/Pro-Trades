90c2c328e3f5e35498f166ccb483b732991c8d74
Tim Davis <timdavish@gmail.com>
2023-12-14 13:07:22 -0800

    chore: formatting config (#41)
    * chore: formatting config
    
    * fix: remove duplicate cors
    
    * fix: cors and allow json body

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PT.Middleware;
 using PT.Services;
-using System.Diagnostics;
 using LogLevel = NLog.LogLevel;
 
 namespace PT
@@ -16,8 +16,9 @@ namespace PT
         // Application config can be read from anywhere in the app
         public static IConfiguration Config { get; set; }
 
-        public static string ExecutingPath { get; set; } = 
-            Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName) ?? Environment.CurrentDirectory;
+        public static string ExecutingPath { get; set; } =
+            Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName)
+            ?? Environment.CurrentDirectory;
 
         public static void Main(string[] args)
         {
@@ -36,12 +37,16 @@ namespace PT
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = Config.GetValue<string>("PostgresConnectionString") ??
-                throw new InvalidOperationException("Connection string 'PostgresConnectionString' not found in config.");
+            var connectionString =
+                Config.GetValue<string>("PostgresConnectionString")
+                ?? throw new InvalidOperationException(
+                    "Connection string 'PostgresConnectionString' not found in config."
+                );
 
             // Add Db
-            builder.Services.AddDbContext<PTContext>(options =>
-                options.UseNpgsql(connectionString));
+            builder
+                .Services
+                .AddDbContext<PTContext>(options => options.UseNpgsql(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             // Add UserService to DI
@@ -62,21 +67,32 @@ namespace PT
             builder.Services.AddSingleton<IHostedService, DataCacheLoader>(); //Load caches in background thread on startup
 
             // Setup the generated swagger JSON for swagger documentation
-            builder.Services.AddSwaggerGen(swagger =>
-            {
-                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Pro-Trades API", Version = "v1" });
-            });
+            builder
+                .Services
+                .AddSwaggerGen(swagger =>
+                {
+                    swagger.SwaggerDoc(
+                        "v1",
+                        new OpenApiInfo { Title = "Pro-Trades API", Version = "v1" }
+                    );
+                });
 
-            // Setup CORS for the serice collection
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(name: Constants.PT_CORS,
-                    policy =>
-                    {
-                        policy.WithOrigins("https://localhost:7777", "https://localhost:7778")
-                            .WithMethods("POST", "GET");
-                    });
-            });
+            // Add CORS (https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-8.0)
+            builder
+                .Services
+                .AddCors(options =>
+                {
+                    options.AddPolicy(
+                        name: Constants.PT_CORS,
+                        policy =>
+                        {
+                            policy
+                                .WithOrigins("http://localhost:7778")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                        }
+                    );
+                });
 
             var app = builder.Build();
 
@@ -96,6 +112,9 @@ namespace PT
             app.UseStaticFiles();
             app.UseRouting();
 
+            // Must be placed after `UseRouting` but before `UseAuthorization`
+            app.UseCors(Constants.PT_CORS);
+
             app.UseAuthentication();
             app.UseAuthorization();
 
@@ -114,9 +133,6 @@ namespace PT
                 swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Pro-Trades API V1");
             });
             app.UseDeveloperExceptionPage();
-
-            // Use CORS with the built app object
-            app.UseCors(Constants.PT_CORS);
 
             app.Run();
         }
